973e09a22ddf5989c5792b7d28920ab2bf529fce
tpmmthomas <good682395@yahoo.com.hk>
2025-10-27 07:00:00 +0800

    Initial commit for MLP 2025-26 Lab 6

--- mlp/optimisers.py
+++ mlp/optimisers.py
@@ -9,8 +9,7 @@ import time
 import logging
 from collections import OrderedDict
 import numpy as np
-import tqdm
-from matplotlib import pyplot as plt
+
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +18,7 @@ class Optimiser(object):
     """Basic model optimiser."""
 
     def __init__(self, model, error, learning_rule, train_dataset,
-                 valid_dataset=None, data_monitors=None, notebook=False):
+                 valid_dataset=None, data_monitors=None):
         """Create a new optimiser instance.
 
         Args:
@@ -44,14 +43,6 @@ class Optimiser(object):
         self.data_monitors = OrderedDict([('error', error)])
         if data_monitors is not None:
             self.data_monitors.update(data_monitors)
-        self.notebook = notebook
-        if notebook:
-            self.tqdm_progress = tqdm.tqdm_notebook
-        else:
-            self.tqdm_progress = tqdm.tqdm
-            
-        self.all_grads = []
-        self.layers = []
 
     def do_training_epoch(self):
         """Do a single training epoch.
@@ -61,50 +52,12 @@ class Optimiser(object):
         respect to all the model parameters and then updates the model
         parameters according to the learning rule.
         """
-        layers = []
-        all_grads = []
-        obtained_grads = False
-        with self.tqdm_progress(total=self.train_dataset.num_batches) as train_progress_bar:
-            train_progress_bar.set_description("Ep Prog")
-            for inputs_batch, targets_batch in self.train_dataset:
-                activations = self.model.fprop(inputs_batch)
-                grads_wrt_outputs = self.error.grad(activations[-1], targets_batch)
-                grads_wrt_params = self.model.grads_wrt_params(
-                    activations, grads_wrt_outputs)
-                self.learning_rule.update_params(grads_wrt_params)
-                train_progress_bar.update(1)
-                if not obtained_grads:
-                    all_grads = []
-                    all_grads.extend(grads_wrt_params)
-                    for i, grad in enumerate(all_grads):
-                        all_grads[i] = np.abs(grad).mean()
-                    layers.extend([f"Layer_{i+1}" for i in range(len(grads_wrt_params))])
-                    obtained_grads = True
-        self.layers.append(layers)
-        self.all_grads.append(all_grads)
-
-    def plot_grad_flow(self):
-        all_grads = self.all_grads
-        layers = self.layers[0] if self.layers else []  # Take first epoch's layers as they don't change
-        
-        
-        # Plot gradient flow for each epoch
-        fig, ax = plt.subplots(figsize=(10, 6))
-        
-        for epoch_idx, epoch_grads in enumerate(all_grads):
-            ax.plot(epoch_grads, alpha=0.3, color="b")
-
-        ax.hlines(0, 0, len(layers), linewidth=1, color="k")
-        ax.set_xticks(range(0, len(layers)))
-        ax.set_xticklabels(layers, rotation="vertical")
-        ax.set_xlim(xmin=0, xmax=len(layers))
-        ax.set_xlabel("Layers")
-        ax.set_ylabel("Average Gradient")
-        ax.set_title("Gradient flow")
-        ax.grid(True)
-        plt.tight_layout()
-        
-        return fig, ax
+        for inputs_batch, targets_batch in self.train_dataset:
+            activations = self.model.fprop(inputs_batch)
+            grads_wrt_outputs = self.error.grad(activations[-1], targets_batch)
+            grads_wrt_params = self.model.grads_wrt_params(
+                activations, grads_wrt_outputs)
+            self.learning_rule.update_params(grads_wrt_params)
 
     def eval_monitors(self, dataset, label):
         """Evaluates the monitors for the given dataset.
@@ -119,7 +72,7 @@ class Optimiser(object):
         data_mon_vals = OrderedDict([(key + label, 0.) for key
                                      in self.data_monitors.keys()])
         for inputs_batch, targets_batch in dataset:
-            activations = self.model.fprop(inputs_batch, evaluation=True)
+            activations = self.model.fprop(inputs_batch)
             for key, data_monitor in self.data_monitors.items():
                 data_mon_vals[key + label] += data_monitor(
                     activations[-1], targets_batch)
@@ -151,7 +104,7 @@ class Optimiser(object):
         """
         logger.info('Epoch {0}: {1:.1f}s to complete\n    {2}'.format(
             epoch, epoch_time,
-            ', '.join(['{}={:.2e}'.format(k, v) for (k, v) in stats.items()])
+            ', '.join(['{0}={1:.2e}'.format(k, v) for (k, v) in stats.items()])
         ))
 
     def train(self, num_epochs, stats_interval=5):
@@ -170,17 +123,14 @@ class Optimiser(object):
         """
         start_train_time = time.time()
         run_stats = [list(self.get_epoch_stats().values())]
-        with self.tqdm_progress(total=num_epochs) as progress_bar:
-            progress_bar.set_description("Exp Prog")
-            for epoch in range(1, num_epochs + 1):
-                start_time = time.time()
-                self.do_training_epoch()
-                epoch_time = time.time()- start_time
-                if epoch % stats_interval == 0:
-                    stats = self.get_epoch_stats()
-                    self.log_stats(epoch, epoch_time, stats)
-                    run_stats.append(list(stats.values()))
-                progress_bar.update(1)
+        for epoch in range(1, num_epochs + 1):
+            start_time = time.time()
+            self.do_training_epoch()
+            epoch_time = time.time() - start_time
+            if epoch % stats_interval == 0:
+                stats = self.get_epoch_stats()
+                self.log_stats(epoch, epoch_time, stats)
+                run_stats.append(list(stats.values()))
         finish_train_time = time.time()
         total_train_time = finish_train_time - start_train_time
         return np.array(run_stats), {k: i for i, k in enumerate(stats.keys())}, total_train_time
